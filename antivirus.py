3e1c95352da2ad3877ed0fa19c01565668b4e8a0
Miquel Comas <mcomash@gmail.com>
2015-10-13 19:58:52 +0200

    Updating GPLv3 warning

--- antivirus.py
+++ antivirus.py
@@ -356,7 +356,11 @@ def detection1():
     if extfiles == []:  
         done1()
 
-lic = "This program is free software: You can redistribute it and/or modify it under the terms of the General Public License version 3"
+lic = """This program is free software: You can redistribute it and/or modify it under the terms of the General Public License version 3
+THERE IS NO WARRANTY FOR THE PROGRAM, TO THE EXTENT PERMITTED BY APPLICABLE LAW.
+EXCEPT WHEN OTHERWISE STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR OTHER PARTIES PROVIDE THE PROGRAM “AS IS” WITHOUT WARRANTY OF ANY KIND,
+EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE PROGRAM IS WITH YOU.
+SHOULD THE PROGRAM PROVE DEFECTIVE, YOU ASSUME THE COST OF ALL NECESSARY SERVICING, REPAIR OR CORRECTION."""
 imp = open("assets/IMPORTANT.txt").read()
 helptxt = open("assets/help.txt").read()
 def gpl():
